a5302f4830d27a7cca3f8494e71ce5ee656c3e8e
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2018-03-24 14:40:00 -0500

    Renamed SQL files to have more descriptive names.
    * Renamed examples tables to match filenames.

--- sql/tables_exs.sqlite.sql
+++ sql/tables_exs.sqlite.sql
@@ -1,9 +1,10 @@
--- Definition of tables to hold medication periods, SQLite3 syntax
+-- Definition of tables to hold examples (medication periods) for
+-- various tasks, SQLite3 syntax
 
 -- Copyright (c) 2018 Aubrey Barnard.  This is free software released
 -- under the MIT License.  See `LICENSE.txt` for details.
 
-create table bupropion_periods (
+create table bupropion_med_periods (
     person_id int,
     "from" text,
     upto text,
@@ -11,7 +12,7 @@ create table bupropion_periods (
     label text
 );
 
-create table duloxetine_periods (
+create table duloxetine_med_periods (
     person_id int,
     "from" text,
     upto text,
