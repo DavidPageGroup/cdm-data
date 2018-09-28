7a9bb3d54da77c0ae5145341e26b94504a0e5e06
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2018-09-27 22:05:34 -0500

    Updated to work with new (SNOMED) data.
    * Added cleaning of filenames.
    * Switched to proper command line arguments.
    * Removed processing of medication periods as their location cannot be
      reliably inferred and they can just be processed in a separate
      cleaning run.
    * Listed files explicitly to eliminate possible errors.  Added warning
      for unrecognized files.
    * Added proper handling of file headers.
    * Wrapped lines of code and comments.
    * Added trailing slashes for `find` so that it handles symbolic links
      properly.

--- bash/clean_sort_compress_data.sh
+++ bash/clean_sort_compress_data.sh
@@ -1,25 +1,36 @@
 # How to clean, sort, and compress the CDM-format data
 
-# Copyright (c) 2018 Aubrey Barnard, Jon Badger.  This is free software released
-# under the MIT License.  See `LICENSE.txt` for details.
+# Copyright (c) 2018 Aubrey Barnard, Jon Badger.  This is free software
+# released under the MIT License.  See `LICENSE.txt` for details.
+
+# Usage: bash clean_sort_compress_data.sh <src> <dst> &> clean_sort_compress_data.$(date +'%Y%m%d-%H%M%S').log
 
 # Exit immediately on errors
 set -e
 
-# Directories given as variables on the command line given prior to the
-# script.  That is, invoke like this:
-# `src_dir=/a/b dst_dir=/c/d bash clean_sort_compress_data.sh &> clean_sort_compress_data.$(date +'%Y%m%d-%H%M%S').log`
-src_dir=${src_dir:=$(pwd)}
-meds_dir=${src_dir}/GenericDrug
-dst_dir=${dst_dir:=${HOME}/cleaned_sorted_data}
-
-# Other directories and variables
+# Essential directories and variables
 prog_name="$(basename ${0})"
 script_dir="$(dirname ${0})"
 
+# Print a timestamped message to stderr
 function log {
     echo "$(date +'%FT%T') ${prog_name}: ${@}" >&2
 }
+
+# Get the command line arguments
+src_dir=${1:-$(pwd)}
+dst_dir=${2:-$(pwd)/clean_data}
+
+# Check the command line arguments
+if [[ ${src_dir} =~ ^--?h(elp)?$ ]]; then
+    echo "Usage: bash ${prog_name} [<src-dir> [<dest-dir>]]"
+    exit 0
+fi
+if [[ ! -d ${src_dir} || ! -r ${src_dir} || ! -x ${src_dir} ]]; then
+    log "ERROR: Not a readable directory: ${src_dir}"
+    exit 1
+fi
+
 log "Starting"
 
 # Make sure destination directory exists
@@ -28,7 +39,11 @@ mkdir -p ${dst_dir}
 # Make sure temporary directory exists and is private
 export TMPDIR=/scratch/${USER}/tmp
 mkdir -p ${TMPDIR}
-chmod go-rwx ${TMPDIR}
+chmod go= ${TMPDIR}
+
+# Make directories absolute
+src_dir=$(cd ${src_dir}; pwd)
+dst_dir=$(cd ${dst_dir}; pwd)
 
 # Time the sorting and compression
 #timer_cmd=
@@ -37,17 +52,38 @@ timer_cmd="/usr/bin/time -v"
 # Sort the data by study ID and compress it (this needs a machine with
 # >120G, many threads, and fast NFS access or else don't run the sorts
 # in parallel or decrease the resources requested)
-log "Starting to clean, sort, and compress CSVs in '${src_dir}' and '${meds_dir}' into '${dst_dir}'"
-for file in $(find ${src_dir} -maxdepth 1 -iname 'omop_*.csv' -not -iname '*no_quote*') $(find ${meds_dir} -maxdepth 1 -iname '*.csv'); do
+log "Starting to clean, sort, and compress CSVs in '${src_dir}' into '${dst_dir}'"
+for file in $(find ${src_dir}/ -maxdepth 1 -iname '*.csv'); do
     base_name=$(basename ${file})
-    dst_file=${dst_dir}/${base_name}
+    # Remove extraneous information from filenames
+    dst_file=${base_name,,} # Make lowercase
+    dst_file=${dst_file/omop_/} # Remove "omop_" prefix
+    dst_file=${dst_file/person_full/person}
+    dst_file=${dst_dir}/${dst_file} # Make full path
     # Set which field is the study ID
-    case ${basename} in
-        (omop_person_full.csv|bupropion*.csv|duloxetine*.csv)
-            sort_keys="--key=1,1n" # First field is study_id
+    case ${base_name} in
+        # Fact tables, first field is numeric ID
+        ( omop_person_full.csv \
+            | omop_care_site.csv \
+            | omop_death.csv \
+            | omop_location.csv \
+            | bupropion*.csv \
+            | duloxetine*.csv )
+            sort_keys="--key=1,1n"
+            ;;
+        # Event tables, second field is subject ID, fourth field is
+        # event date
+        ( omop_condition_occurrence.csv \
+            | omop_drug_exposure.csv \
+            | omop_measurement.csv \
+            | omop_observation.csv \
+            | omop_procedure_occurrence.csv \
+            | omop_visit_occurrence.csv )
+            sort_keys="--key=2,2n --key=4,4"
             ;;
         (*)
-            sort_keys="--key=2,2n --key=4,4" # Second field is study_id, fourth field is *_date
+            log "WARNING: Skipping processing unrecognized file: '${file}'"
+            continue
             ;;
     esac
     # Clean up double quotes in literal-format CSV files
@@ -61,9 +97,18 @@ for file in $(find ${src_dir} -maxdepth 1 -iname 'omop_*.csv' -not -iname '*no_q
         log "Starting to clean and sort '${file}'"
         # Change to Unix EOLs (delete "CR" from "CRLF"; `dos2unix`
         # doesn't work as a filter) and clean the fields.  Then sort by
-        # patient ID.  For GNU Awk (GAWK) version < 4.0 --re-interval is required
-        # to accomodate regular expressions with intervals.
-        ${timer_cmd} sed -e 's/\r//g' ${sub_dblqt} ${file} | ${timer_cmd} awk --re-interval  -F , -f ${script_dir}/clean_data.awk | ${timer_cmd} sort --stable --field-separator=, --buffer-size=10G --temporary-directory=${TMPDIR} ${sort_keys} > ${dst_file}
+        # patient ID.  For GNU Awk (GAWK) version < 4.0 --re-interval is
+        # required to accomodate regular expressions with intervals.
+        # Remove the header before processing and reinstate it after.
+        ${timer_cmd} tail -n +2 ${file} \
+            | ${timer_cmd} sed -e 's/\r//g' ${sub_dblqt} \
+            | ${timer_cmd} awk --re-interval  -F , \
+                  -f ${script_dir}/clean_data.awk \
+            | ${timer_cmd} sort --stable --field-separator=, \
+                  --buffer-size=10G --temporary-directory=${TMPDIR} \
+                  ${sort_keys} \
+            | cat <(head -n 1 ${file} \
+                        | sed -e 's/\r//g') - > ${dst_file}
         log "Done cleaning and sorting '${file}'"
         # Compress in various ways to allow for different size /
         # decompress speed trade-offs.  Do all compression in parallel.
@@ -86,7 +131,7 @@ log "Checking for remaining dirt"
 # and other dirt.  Turn off exiting on error because `grep` returns 1
 # when it does not find a match (which is good in this case).
 set +e
-for file in $(find ${dst_dir} -maxdepth 1 -iname '*.csv'); do
+for file in $(find ${dst_dir}/ -maxdepth 1 -iname '*.csv'); do
     # Print lines with an odd number of double quotes.  These are the
     # lines to check for proper CSV syntax.  Using `grep` as below is
     # faster than `awk -F '"' '{if (((NF - 1) % 2) != 0) print}'`.
@@ -98,6 +143,6 @@ for file in $(find ${dst_dir} -maxdepth 1 -iname '*.csv'); do
 done
 wait
 # Remove empty files (which leaves only those files with actual dirt)
-find ${dst_dir} -empty -delete
+find ${dst_dir}/ -type f -empty -delete
 log "Done checking for dirt"
 log "All done"
