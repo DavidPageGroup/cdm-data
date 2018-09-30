243998c5d7c3f28be64739151fe85d5ab3abac36
Aubrey Barnard <aubrey.f.barnard@gmail.com>
2018-09-30 18:07:21 -0500

    Updated SQLite DDL files for 20180705.snomed version of data.
    * Added new tables: death, observation, care_site, location.
    * Added / Removed columns where appropriate.
    * Updated indexes accordingly.

--- sql/indexes_emr.sqlite.sql
+++ sql/indexes_emr.sqlite.sql
@@ -3,6 +3,10 @@
 -- Copyright (c) 2018 Aubrey Barnard.  This is free software released
 -- under the MIT License.  See `LICENSE.txt` for details.
 
+
+-- Clinical data tables
+-- (follows order in `tables_emr.sqlite.sql`)
+
 -- person
 create index idx_person__pt  on person (person_id);
 create index idx_person__sex on person (gender_concept_id);
@@ -13,16 +17,31 @@ create index idx_cond_occur__pt on condition_occurrence (person_id);
 create index idx_cond_occur__id on condition_occurrence (condition_concept_id);
 create index idx_cond_occur__dt on condition_occurrence (condition_start_date);
 
+-- death
+create index idx_death__pt on death (person_id);
+create index idx_death__id on death (death_type_concept_id);
+create index idx_death__dt on death (death_date);
+create index idx_death__cause on death (cause_concept_id);
+
 -- drug_exposure
 create index idx_drug_exp__pt on drug_exposure (person_id);
 create index idx_drug_exp__id on drug_exposure (drug_concept_id);
-create index idx_drug_exp__type_id on drug_exposure (drug_type_concept_id); 
 create index idx_drug_exp__dt on drug_exposure (drug_exposure_start_date);
+create index idx_drug_exp__type on drug_exposure (drug_type_concept_id);
 
 -- measurement
 create index idx_measrmnt__pt on measurement (person_id);
 create index idx_measrmnt__id on measurement (measurement_concept_id);
 create index idx_measrmnt__dt on measurement (measurement_date);
+create index idx_measrmnt__type on measurement (measurement_type_concept_id);
+create index idx_measrmnt__val on measurement (value_as_concept_id);
+
+-- observation
+create index idx_obs__pt on observation (person_id);
+create index idx_obs__id on observation (observation_concept_id);
+create index idx_obs__dt on observation (observation_date);
+create index idx_obs__type on observation (observation_type_concept_id);
+create index idx_obs__val on observation (value_as_concept_id);
 
 -- procedure_occurrence
 create index idx_proc_occur__pt on procedure_occurrence (person_id);
@@ -33,3 +52,10 @@ create index idx_proc_occur__dt on procedure_occurrence (procedure_date);
 create index idx_visit_occur__pt on visit_occurrence (person_id);
 create index idx_visit_occur__id on visit_occurrence (visit_concept_id);
 create index idx_visit_occur__dt on visit_occurrence (visit_start_date);
+
+
+-- Health system data tables
+
+create index idx_care_site__id on care_site (care_site_id);
+
+create index idx_location__id on location (location_id);
--- sql/tables_emr.sqlite.sql
+++ sql/tables_emr.sqlite.sql
@@ -14,6 +14,10 @@
 
 -- Omit column constraints (e.g. not null) because data is messy
 
+
+-- Clinical data tables
+-- (alphabetical except `person` first)
+
 create table person (
     person_id int,
     gender_concept_id int,
@@ -40,13 +44,27 @@ create table condition_occurrence (
     person_id int,
     condition_concept_id int,
     condition_start_date text,
+    condition_start_datetime text,
     condition_end_date text,
+    condition_end_datetime text,
     condition_type_concept_id int,
     stop_reason text,
     provider_id int,
     visit_occurrence_id int,
     condition_source_value text,
-    condition_source_concept_id int
+    condition_source_concept_id int,
+    condition_status_source_value text,
+    condition_status_concept_id int
+);
+
+create table death (
+    person_id int,
+    death_date text,
+    death_datetime text,
+    death_type_concept_id int,
+    cause_concept_id int,
+    cause_source_value text,
+    cause_source_concept_id int
 );
 
 create table drug_exposure (
@@ -54,7 +72,10 @@ create table drug_exposure (
     person_id int,
     drug_concept_id int,
     drug_exposure_start_date text,
+    drug_exposure_start_datetime text,
     drug_exposure_end_date text,
+    drug_exposure_end_datetime text,
+    verbatim_end_date text,
     drug_type_concept_id int,
     stop_reason text,
     refills int,
@@ -62,8 +83,6 @@ create table drug_exposure (
     days_supply int,
     sig text,
     route_concept_id int,
-    effective_drug_dose real,
-    dose_unit_concept_id int,
     lot_number text,
     provider_id int,
     visit_occurrence_id int,
@@ -78,7 +97,7 @@ create table measurement (
     person_id int,
     measurement_concept_id int,
     measurement_date text,
-    measurement_time text,
+    measurement_datetime text,
     measurement_type_concept_id int,
     operator_concept_id int,
     value_as_number num,
@@ -94,11 +113,32 @@ create table measurement (
     value_source_value text
 );
 
+create table observation (
+    observation_id int,
+    person_id int,
+    observation_concept_id int,
+    observation_date text,
+    observation_datetime text,
+    observation_type_concept_id int,
+    value_as_number num,
+    value_as_string text,
+    value_as_concept_id int,
+    qualifier_concept_id int,
+    unit_concept_id int,
+    provider_id int,
+    visit_occurrence_id int,
+    observation_source_value text,
+    observation_source_concept_id int,
+    unit_source_value text,
+    qualifier_source_value text
+);
+
 create table procedure_occurrence (
     procedure_occurrence_id int,
     person_id int,
     procedure_concept_id int,
     procedure_date text,
+    procedure_datetime text,
     procedure_type_concept_id int,
     modifier_concept_id int,
     quantity int,
@@ -114,12 +154,40 @@ create table visit_occurrence (
     person_id int,
     visit_concept_id int,
     visit_start_date text,
-    visit_start_time text,
+    visit_start_datetime text,
     visit_end_date text,
-    visit_end_time text,
+    visit_end_datetime text,
     visit_type_concept_id int,
     provider_id int,
     care_site_id int,
+    admitting_source_concept_id int,
+    discharge_to_concept_id int,
+    preceding_visit_occurrence_id int,
     visit_source_value text,
-    visit_source_concept_id int
+    visit_source_concept_id int,
+    admitting_source_value text,
+    discharge_to_source_value text
+);
+
+
+-- Health system data tables
+
+create table care_site (
+    care_site_id int,
+    care_site_name text,
+    place_of_service_concept_id int,
+    location_id int,
+    care_site_source_value text,
+    place_of_service_source_value text
+);
+
+create table location (
+    location_id int,
+    address_1 text,
+    address_2 text,
+    city text,
+    state text,
+    zip text,
+    county text,
+    location_source_value text
 );
